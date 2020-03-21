75cc783c36c722e47882387d39cc95743bb5364a
Gyorgy Pakozdi <gyorgy.pakozdi@supercharge.io>
2020-03-21 15:44:12 +0100

    :rocket:

--- agent.py
+++ agent.py
@@ -5,8 +5,8 @@ import os
 publish = 'git add . && git commit --amend -C HEAD && git push -f > /dev/null'
 
 while True:
-    print('{}   render f44d'.format(time.ctime()))
+    print('[{}] render f44d'.format(time.ctime()))
     render.render()
-    print('{}   publish f44d'.format(time.ctime()))
+    print('[{}] publish f44d'.format(time.ctime()))
     os.system(publish)
-    time.sleep(60)
+    time.sleep(60 * 5)
