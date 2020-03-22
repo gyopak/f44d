4e6190df5ff045d4c35added058ac9cf86ff065b
Gyorgy Pakozdi <gyorgy.pakozdi@supercharge.io>
2020-03-22 14:26:27 +0100

    :rocket:

--- agent.py
+++ agent.py
@@ -2,11 +2,18 @@ import time
 import render
 import os
 
-publish = 'git fetch --all && git reset --hard origin master && git add . && git commit --amend -C HEAD && git push -f > /dev/null'
+clean = "git reset --hard origin/master && git pull"
+publish = 'git add . && git commit --amend -C HEAD && git push -f > /dev/null'
 
+def log(l):
+    print('[{}] {}'.format(time.ctime(), l))
+ 
 while True:
-    print('[{}] render f44d'.format(time.ctime()))
+    log('clean env')
+    os.system(clean)
+    log('render 444')
     render.render()
-    print('[{}] publish f44d'.format(time.ctime()))
+    log('publish 444')
     os.system(publish)
+    log('sleep 5mins')
     time.sleep(60 * 5)
